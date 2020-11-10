821ed1a532c0024aa35e902a314a9f738cf6104f
IM\asolovyeva <asolovyeva@company.dnevnik.ru>
2020-11-11 00:49:54 +0300

    Unit

---
+++ specs/calculator2.spec.js
@@ -0,0 +1,23 @@
+import calculator from '../src/calculator';
+describe('Calculator 100% coverage', () => {
+  test('it loads without error', () => {
+    expect(calculator).toBeDefined();
+    expect(typeof calculator).toBe('function');
+  });
+  test.each`
+    a      | op     | b      | expected
+    ${2}   | ${'*'} | ${2}   | ${4}
+    ${2}   | ${'/'} | ${2}   | ${1}
+    ${2}   | ${'+'} | ${2}   | ${4}
+    ${2}   | ${'-'} | ${2}   | ${0}
+    ${'?'} | ${'+'} | ${2}   | ${'error'}
+    ${2}   | ${'?'} | ${2}   | ${'error'}
+    ${2}   | ${'+'} | ${'?'} | ${'error'}
+  `('$a $op $b = $expected', ({ a, op, b, expected }) => {
+    if (expected === 'error') {
+      expect(() => calculator(a, op, b)).toThrow();
+    } else {
+      expect(calculator(a, op, b)).toBe(expected);
+    }
+  });
+});
